133f076a6a25271f0cb090e97d05b94c3e6934ea
micazanello <micazanello@gmail.com>
2025-07-18 19:17:44 -0300

    CAMBIOS FINALES

--- ZanelloMicaela-TP-WEB/scripts.js
+++
@@ -1,5 +0,0 @@
-// Cambiar navbar al hacer scroll
-window.addEventListener('scroll', function() {
-  const navbar = document.querySelector('.navbar');
-  navbar.classList.toggle('scrolled', window.scrollY > 50);
-});
